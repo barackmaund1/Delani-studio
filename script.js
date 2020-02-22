0fc4f9160c2431b05e821489e1a301dc0638ecb9
barackmaund1 <pmaundu49@gmail.co>
2020-02-22 08:39:33 +0300

    add features of QJuery

--- script.js
+++ script.js
@@ -6,9 +6,15 @@ $(document).ready(function() {
     $(".development").click(function(effect) {
         $("#development1").toggle();
         $("#development2").toggle();
-    })
+    });
     $(".product").click(function(effect) {
         $("#product1").toggle();
         $("#product2").toggle();
     })
+    $(".image").mouseover(function(effect) {
+        $(this).css("opacity","0.5")
+    }).mouseout(function(){
+        $(this).css("opacity","1")
+    });
+
 })
