9e79a2b511e45cfcd7ca5568bdcf2bf83b684518
barackmaund1 <pmaundu49@gmail.co>
2020-02-22 03:37:33 +0300

    add script.js content

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $(".design").click(function(effect) {
+        $("#design1").toggle();
+        $("#design2").toggle();
+    })
+    $(".development").click(function(effect) {
+        $("#development1").toggle();
+        $("#development2").toggle();
+    })
+    $(".product").click(function(effect) {
+        $("#product1").toggle();
+        $("#product2").toggle();
+    })
+})
